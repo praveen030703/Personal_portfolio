49c2255c4c8f67e2eea3ad332a74a3e9969f5496
praveen030703 <praveenrs1503@gmail.com>
2025-07-25 15:21:50 +0530

    mywork_data changes

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,9 +1,9 @@
 import project1_img from "../assets/MovieReview.png";
 import project2_img from "../assets/project2_img.png";
-import project3_img from "../assets/project_3.svg";
-import project4_img from "../assets/project_4.svg";
-import project5_img from "../assets/project_5.svg";
-import project6_img from "../assets/project_6.svg";
+// import project3_img from "../assets/project_3.svg";
+// import project4_img from "../assets/project_4.svg";
+// import project5_img from "../assets/project_5.svg";
+// import project6_img from "../assets/project_6.svg";
 
 const mywork_data = [
   {
